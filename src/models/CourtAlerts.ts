0ba2a5707cf10673ff28e77112e112db996df577
Kyle Harris <kyle@learnwithjuni.com>
2022-12-11 20:15:26 -0800

    add example doc to make instert easier

--- src/models/CourtAlerts.ts
+++ src/models/CourtAlerts.ts
@@ -19,3 +19,32 @@ const CourtAlertsSchema = new Schema<ICourtAlerts>({
 });
 
 export const CourtAlerts = model<ICourtAlerts>('CourtAlerts', CourtAlertsSchema);
+
+/*
+Example starter doc
+
+{
+  "_id": {
+    "$oid": "6396a7aeb17ab9a6170cd82a"
+  },
+  "userId": "kyle",
+  "courtId": "4441573",
+  "status": "new",
+  "date": {
+    "$date": {
+      "$numberLong": "1670803200000"
+    }
+  },
+  "startTime": {
+    "$date": {
+      "$numberLong": "1670864400000"
+    }
+  },
+  "endTime": {
+    "$date": {
+      "$numberLong": "1670878800000"
+    }
+  }
+}
+
+*/
